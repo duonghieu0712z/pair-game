bda0a7a8f905c1143abf787d1a67eb773d32c221
Duong Hieu <duonghieu0712z@gmail.com>
2023-11-22 09:38:50 +0700

    Disable shuffle

--- game/utils.js
+++ game/utils.js
@@ -1,7 +1,7 @@
 export function shuffle(array) {
-  for (let index = array.length - 1; index > 0; index--) {
-    const randomIndex = Math.trunc(Math.random() * (index + 1));
-    [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
-  }
+  // for (let index = array.length - 1; index > 0; index--) {
+  //   const randomIndex = Math.trunc(Math.random() * (index + 1));
+  //   [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+  // }
   return array;
 }
